16f622bacbfbd126255b4217477ba7b48f3fd088
Felipe Salinas <fea.salinas@duocuc.cl>
2025-09-07 22:55:24 -0300

    Primer view y url

--- juego_forest/juego_forest/urls.py
+++ juego_forest/juego_forest/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from core.views import menuprincipal_wiki
+
 urlpatterns = [
     #path('admin/', admin.site.urls),
+    path("", menuprincipal_wiki, name="menuprincipa_wiki"),
 ]
